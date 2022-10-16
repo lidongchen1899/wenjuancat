6ad763c347ee946bfdac9fe1012d4dae27ba9bb6
lidongchen <lidongchen012@163.com>
2022-10-16 20:58:10 +0800

    first commit

---
+++ wjcat/src/components/Index.vue
@@ -0,0 +1,95 @@
+<!--
+程序名：网站首页
+功能：对网站进行介绍
+-->
+<template>
+  <div class="main">
+    <el-carousel height="360px">
+      <el-carousel-item v-for="(item, index) in topImg" :key="index">
+        <img :src="item.url" style="width: 100%;height: 100%;" />
+        <div class="cover">
+          {{ item.title }}<br />
+          <span style="font-size: 35px">{{ item.subTitle }}</span>
+        </div>
+      </el-carousel-item>
+    </el-carousel>
+    <el-divider></el-divider>
+
+    <!-- <code-div><img src="/static/images/demo1.png" class="demoImg"/></code-div> -->
+    <code-div><img src="/static/images/demo2.png" class="demoImg"/></code-div>
+    <code-div><img src="/static/images/demo3.png" class="demoImg"/></code-div>
+    <!-- <img src="/static/images/demo1.png" class="demoImg" /><br /> -->
+    <!-- <img src="/static/images/demo2.png" class="demoImg" /><br />
+    <img src="/static/images/demo3.png" class="demoImg" /><br /><br /> -->
+    
+    <el-footer class="bottom">
+      <p>Copyright © 2021 国防大学. All rights reserved.</p>
+    </el-footer>
+  </div>
+</template>
+<script>
+import CodeDiv from "@/components/CodeDiv";
+export default {
+  components: {
+    CodeDiv
+  },
+  data() {
+    return {
+      topImg: [
+        {
+          title: "",
+          subTitle: "",
+          url: "/static/images/1.jpg"
+        },
+                 {
+                   'title':'',
+                   'subTitle':'',
+                   'url':'/static/images/2.jpg'
+                 },
+                 {
+                   'title':'',
+                   'subTitle':'',
+                   'url':'/static/images/3.jpg'
+                 },
+      ]
+    };
+  }
+};
+</script>
+<style scoped>
+.main {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+.bottom {
+  height: 150px !important;
+  background-color: #2e3e4e;
+  color: #9b9ea0;
+  position: relative;
+  padding: 20px;
+}
+.demoImg {
+  width: 100%;
+  height: 100%;
+  /*border-radius: 5px;*/
+}
+.cover {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  /* background-color: rgba(0, 0, 0, 0.6); */
+  z-index: 100;
+  left: 0;
+  top: 0;
+  color: white;
+  font-size: 50px;
+  line-height: 60px;
+  padding-top: 180px;
+}
+.lj {
+}
+.b {
+  font-weight: 700;
+}
+</style>
